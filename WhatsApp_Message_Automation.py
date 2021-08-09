63a8bfa7015c95f4403630b6ab55d33d782f8330
Mohit Pandey <67647538+mohit377@users.noreply.github.com>
2021-08-09 02:52:48 -0700

    Add files via upload

---
+++ WhatsApp_Message_Automation.py
@@ -0,0 +1,17 @@
+# Import module
+import pywhatkit
+# To Download Module.
+# Open Terminal or CMD and type
+# Command >>> pip install pywhatkit
+# Hit Enter
+# Now Send Message
+pywhatkit.sendwhatmsg("Your phone number with country code", "Your Message", 1, 10)
+# Example >>> pywhatkit.sendwhatmsg("+911234567890", "WhatsApp Automation using Python.", 1, 10) 1 Hour, 10 Minutes
+# You can change time.
+# Programmer - Mohit Pandey
+# YouTube - https://youtube.com/c/mohitpandey10
+# Follow me on GitHub and SUBSCRIBE YouTube Channel.
+# Thankyou.
+# Have a nice day!!!
+
+
